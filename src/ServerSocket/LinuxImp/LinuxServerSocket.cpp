6513a1d56d4b1bd3fd390af3f6d2e0335892c6de
hhhh hgfh <dania.lukin@gmail.com>
2024-12-26 00:21:06 +0300

    feat(1): add class LinuxServerSocket

---
+++ src/ServerSocket/LinuxImp/LinuxServerSocket.cpp
@@ -0,0 +1,43 @@
+#include "LinuxServerSocket.h"
+#include <LinuxClientSocket.h>
+
+int LinuxServerSocket::Configuration(int protocol) {
+    _socket = socket(AF_INET, SOCK_STREAM, protocol);
+    if (_socket == INVALID_SOCKET){
+        Close();
+        return INVALID_SOCKET;
+    }
+    return 0;
+}
+
+int LinuxServerSocket::Bind(const int port, const char *address) {
+    _sockAddr.sin_family = AF_INET;
+    _sockAddr.sin_addr.s_addr = inet_addr(address);
+    _sockAddr.sin_port = htons(port);
+
+    if (bind(_socket, (sockaddr*)&_sockAddr, sizeof(_sockAddr)) == SOCKET_ERROR)
+        return SOCKET_ERROR;
+    return 0;
+}
+
+int LinuxServerSocket::Listen(int backLog) {
+    if (listen(_socket, backLog) == SOCKET_ERROR)
+        return SOCKET_ERROR;
+    return 0;
+}
+
+IClientSocket *LinuxServerSocket::Accept() {
+    int client;
+    client = accept(_socket, nullptr, nullptr);
+
+    if (client == INVALID_SOCKET)
+        return nullptr;
+
+    LinuxClientSocket* clientSocket = new LinuxClientSocket();
+    clientSocket->lConfiguration(client);
+    return clientSocket;
+}
+
+void LinuxServerSocket::Close() {
+    close(_socket);
+}
---
+++ src/ServerSocket/LinuxImp/LinuxServerSocket.h
@@ -0,0 +1,23 @@
+#ifndef CSOCKET_LINUXSERVERSOCKET_H
+#define CSOCKET_LINUXSERVERSOCKET_H
+
+#include <IServerSocket.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+class LinuxServerSocket : public IServerSocket{
+private:
+    int _socket;
+    sockaddr_in _sockAddr;
+public:
+    int Configuration(int protocol) override;
+    int Bind(const int port, const char* address) override;
+    int Listen(int backLog) override;
+    IClientSocket* Accept() override;
+    void Close() override;
+};
+
+
+#endif //CSOCKET_LINUXSERVERSOCKET_H
