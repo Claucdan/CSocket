584c06d23fed0b481594aba199b780d97cd59973
hhhh hgfh <dania.lukin@gmail.com>
2025-01-12 22:09:43 +0300

    feat(6): added internal libraries to split the shared library into smaller ones

--- src/CSocket.h
+++ src/CSocket.h
@@ -1,17 +1,7 @@
 #ifndef CSOCKET_CSOCKET_H
 #define CSOCKET_CSOCKET_H
 
-#include "SocketDefinitionTypes.h"
-
-#include "ClientSockets/IClientSocket.h"
-#include <ServerSocket/IServerSocket.h>
-
-#if WIN32
-#include "ClientSockets/WinsockImp/WindowsClientSocket.h"
-#include "ServerSocket/WindowsImp/WindowsServerSocket.h"
-#else
-#include "ClientSockets/LinuxImp/LinuxClientSocket.h"
-#include "ServerSocket/LinuxImp/LinuxServerSocket.h"
-#endif
+#include <ClientSocket.h>
+#include <ServerSocket.h>
 
 #endif //CSOCKET_CSOCKET_H
